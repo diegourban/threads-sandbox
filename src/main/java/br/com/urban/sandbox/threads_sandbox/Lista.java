e687883523795282be42c8156a71feb0838bfd3c
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-09-21 12:41:15 -0300

    Update Lista.java

--- src/main/java/br/com/urban/sandbox/threads_sandbox/Lista.java
+++ src/main/java/br/com/urban/sandbox/threads_sandbox/Lista.java
@@ -1,1 +1,36 @@
+package br.com.urban.sandbox.threads_sandbox;
+
+public class Lista {
+
+	private String[] elementos = new String[100];
+	private int indice = 0;
+
+	public synchronized void adicionaElementos(String elemento) {
+		this.elementos[indice] = elemento;
+		this.indice++;
+
+		try{
+            Thread.sleep(10);
+        } catch(InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        if (this.indice == this.tamanho()) {
+            System.out.println("lista tá cheia, notificando");
+            this.notify();
+        }
+	}
+
+	public int tamanho() {
+		return this.elementos.length;
+	}
+
+	public String pegaElemento(int posicao) {
+		return this.elementos[posicao];
+	}
+
+	public boolean estaCheia() {
+		return this.indice == this.tamanho();
+	}
+}
 
