5a50458641c1181298fb36c0413242988f3e8278
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-09-21 12:41:56 -0300

    Update TarefaAdicionarElementos.java

--- src/main/java/br/com/urban/sandbox/threads_sandbox/TarefaAdicionarElementos.java
+++ src/main/java/br/com/urban/sandbox/threads_sandbox/TarefaAdicionarElementos.java
@@ -1,28 +1,28 @@
 package br.com.urban.sandbox.threads_sandbox;
 
-import java.util.List;
-
 public class TarefaAdicionarElementos implements Runnable {
 
-	//private Lista lista;
-	private List<String> lista;
 	private int numeroDoThread;
 
-//	public TarefaAdicionarElementos(Lista lista, int numeroDoThread) {
-//		this.lista = lista;
-//		this.numeroDoThread = numeroDoThread;
-//	}
+	private Lista lista;
+	public TarefaAdicionarElementos(Lista lista, int numeroDoThread) {
+		this.lista = lista;
+		this.numeroDoThread = numeroDoThread;
+	}
 	
+	/*
+	private List<String> lista;
 	public TarefaAdicionarElementos(List<String> lista, int numeroDoThread) {
 		this.lista = lista;
 		this.numeroDoThread = numeroDoThread;
 	}
+	*/
 
 	@Override
 	public void run() {
 		for (int i = 0; i < 10; i++) {
-			//lista.adicionaElementos("Thread " + numeroDoThread + " - " + i);
-			lista.add("Thread " + numeroDoThread + " - " + i);
+			lista.adicionaElementos("Thread " + numeroDoThread + " - " + i);
+			//lista.add("Thread " + numeroDoThread + " - " + i);
 		}
 	}
 }
