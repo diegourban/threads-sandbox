59ed679318363e2cd0be2cbc5b89e266a89207ab
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-09-21 20:06:10 -0300

    server project

---
+++ src/main/java/br/com/urban/sandbox/threads_sandbox/ClienteTarefa.java
@@ -0,0 +1,22 @@
+package br.com.urban.sandbox.threads_sandbox;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class ClienteTarefa {
+
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		Socket socket = new Socket("localhost", 12345);
+		
+		System.out.println("Conexão estabelecida");
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		scanner.nextLine();
+		
+		socket.close();
+	}
+
+}
---
+++ src/main/java/br/com/urban/sandbox/threads_sandbox/ServidorTarefas.java
@@ -0,0 +1,23 @@
+package br.com.urban.sandbox.threads_sandbox;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServidorTarefas {
+
+	public static void main(String[] args) throws IOException {
+		System.out.println("Iniciando servidor...");
+		ServerSocket servidor = new ServerSocket(12345);
+		
+		while(true){
+			Socket socket = servidor.accept();
+			System.out.println("Aceitou novo cliente na porta " + socket.getPort());
+			
+			DistribuirTarefas distribuirTarefas = new DistribuirTarefas(socket);
+			Thread threadClient = new Thread(distribuirTarefas);
+			threadClient.start();
+		}
+	}
+
+}
