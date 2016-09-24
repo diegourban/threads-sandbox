cbc3dacbf76a1be3d07d8a12fee2a69f3c8d96d8
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-09-23 21:47:25 -0300

    SErvidor de Teste

---
+++ src/main/java/br/com/urban/sandbox/threads_sandbox/threads2/servidor/ServidorDeTeste.java
@@ -0,0 +1,35 @@
+package br.com.urban.sandbox.threads_sandbox.threads2.servidor;
+
+public class ServidorDeTeste {
+
+	// atributo e main comentados
+	private boolean estaRodando;
+
+	private void rodar() {
+		try {
+			new Thread(new Runnable() {
+
+				public void run() {
+					System.out.println("Servidor começando, estaRodando = " + estaRodando);
+					while (!estaRodando) {
+					}
+
+					if (estaRodando) {
+						throw new RuntimeException("Deu erro na thread...");
+					}
+
+					System.out.println("Servidor rodando, estaRodando = " + estaRodando);
+
+					while (estaRodando) {
+					}
+
+					System.out.println("Servidor terminando, estaRodando = " + estaRodando);
+				}
+			}).start();
+		} catch (Exception e) {
+			System.out.println("Catch na thread MAIN " + e.getMessage());
+		}
+	}
+
+	// método alterandoAtributo comentado
+}
